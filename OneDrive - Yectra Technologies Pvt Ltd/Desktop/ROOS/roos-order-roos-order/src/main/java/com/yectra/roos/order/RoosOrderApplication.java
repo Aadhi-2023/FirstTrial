b5255e1ea9af73296949a8a087ad4867e5c30814
Aadhi <121472906+VAadhi@users.noreply.github.com>
2023-09-12 15:13:19 +0530

    Roos - Order Files

--- OneDrive - Yectra Technologies Pvt Ltd/Desktop/ROOS/roos-order-roos-order/src/main/java/com/yectra/roos/order/RoosOrderApplication.java
+++ OneDrive - Yectra Technologies Pvt Ltd/Desktop/ROOS/roos-order-roos-order/src/main/java/com/yectra/roos/order/RoosOrderApplication.java
@@ -2,8 +2,6 @@ package com.yectra.roos.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
 import com.azure.spring.data.cosmos.repository.config.EnableCosmosRepositories;
 
 @SpringBootApplication
